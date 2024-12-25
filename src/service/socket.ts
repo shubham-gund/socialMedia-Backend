507acef59c4f9f93a8e477ee21b25dbf57ff0eb7
shubham-gund <shubhamgund91@gmail.com>
2024-12-25 14:11:04 +0530

    fixed realtime messaging issue

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -1,12 +1,11 @@
+// socket.ts
 import { Server as SocketIOServer } from "socket.io";
 import http from "http";
-import express from "express"
-
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
 
-// This will be imported and used in the main server (server.ts)
 const io = new SocketIOServer(server, {
   cors: {
     origin: [
@@ -18,14 +17,12 @@ const io = new SocketIOServer(server, {
   },
 });
 
-// User socket map
 interface UserSocketMap {
   [userId: string]: string;
 }
 
-const userSocketMap: UserSocketMap = {}; // Map of userId to socketId
+const userSocketMap: UserSocketMap = {};
 
-// Function to get receiver's socket ID
 export function getReceiverSocketId(userId: string): string | undefined {
   return userSocketMap[userId];
 }
@@ -34,14 +31,21 @@ io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
   const userId = socket.handshake.query.userId as string;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  }
 
-  if (userId)  userSocketMap[userId] = socket.id; 
-
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // Handle new message event
+  socket.on("sendMessage", (message) => {
+    const receiverSocketId = getReceiverSocketId(message.receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", message);
+    }
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
-    
     if (userId) {
       delete userSocketMap[userId];
       io.emit("getOnlineUsers", Object.keys(userSocketMap));
